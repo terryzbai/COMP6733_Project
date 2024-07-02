c4c97769da1268c1b39e8c6340229d991b96ff63
杨晨 <yangchen@s-MacBook-Air.local>
2024-07-02 14:12:33 +1000

    record_g_code

---
+++ src/record_data.py
@@ -0,0 +1,20 @@
+import time
+import imu
+from machine import Pin, I2C, Timer
+
+bus = I2C(1, scl=Pin(15), sda=Pin(14))
+imu = imu.IMU(bus)
+
+
+
+def mycallback(t):
+
+    print('{:>.f},{:>.f},{:>.f}'.format(*imu.accel())+','+'{:>.f},{:>.f},{:>.f}'.format(*imu.gyro()))
+    
+    
+tim = Timer(1, period=10000, mode=Timer.PERIODIC,callback=mycallback)
+tim.start()
+
+
+time.sleep_ms(1000)
+tim.stop()
