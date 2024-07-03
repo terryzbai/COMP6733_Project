6a556e84069d3afcf67cf8f9181f26902d5fa405
J0020git <ralph.jerome.hernandez@outlook.com>
2024-07-03 22:22:35 +1000

    fix: Added Bluetooth address for Govee bulb

--- src/ble/known_devices.py
+++ src/ble/known_devices.py
@@ -1,9 +1,9 @@
 import platform
 
 GOVEE_BULB_ADDRESS = (
-    # Bluetooth address on Linux/Windows
-    "..."
+    # MAC address on Linux/Windows
+    "60:74:F4:EC:C8:A9"
     if platform.system() != "Darwin"
     else "EFA9751B-E8BE-FCAA-24CA-75130F3DD64E"
-    # MAC address on macOS
+    # Bluetooth address on macOS
 )
