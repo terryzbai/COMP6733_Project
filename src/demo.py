2b9ee633f4889b602407527595d3334665894a8e
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-08-01 17:16:11 +1000

    fix: Code cleanup and mapping ID to name

--- src/demo.py
+++ src/demo.py
@@ -3,94 +3,124 @@ import joblib
 import numpy as np
 from feature_extraction import GestureFeature
 
-sampling_rate = 100
-min_gus_time = 25
-max_gus_time = 200
-thredhold = 500
-mang_change = 70
+# Configuration
+SAMPLING_RATE = 100
+MIN_GESTURE_TIME = 25
+MAX_GESTURE_TIME = 200
+GYRO_THRESHOLD = 500
+GYRO_CHANGE_THRESHOLD = 70
+GESTURES = {1: "Snap", 2: "Clockwise", 3: "Counterclockwise", 4: "Up", 5: "Down"}
 
-# 假设 X_test 已经定义
-# 加载模型
-clf = joblib.load('./model/vf.pkl')
+# Load the pre-trained machine learning model
+clf = joblib.load("./model/rf.pkl")
 
-# 进行预测
-# y_pred = clf.predict(X_test)
 
-def getFeatures(data):
-    gf = GestureFeature(data, sampling_rate)
-
-    features = np.concatenate((
-        gf.ACEnergy(),
-        gf.ACLowEnergy(),
-        gf.DCMean(),
-        gf.DCTotalMean(),
-        gf.DCArea(),
-        gf.DCPostureDist(),
-        gf.ACAbsMean(),
-        gf.ACAbsArea(),
-        gf.ACTotalAbsArea(),
-        gf.ACVar(),
-        gf.ACAbsCV(),
-        gf.ACIQR(),
-        gf.ACRange_per_axis(),
-        np.array([len(data)])
-    ))
+def extract_features(data):
+    """
+    Extracts features from the input data
+    """
+    gf = GestureFeature(data, SAMPLING_RATE)
+    features = np.concatenate(
+        [
+            gf.ACEnergy(),
+            gf.ACLowEnergy(),
+            gf.DCMean(),
+            gf.DCTotalMean(),
+            gf.DCArea(),
+            gf.DCPostureDist(),
+            gf.ACAbsMean(),
+            gf.ACAbsArea(),
+            gf.ACTotalAbsArea(),
+            gf.ACVar(),
+            gf.ACAbsCV(),
+            gf.ACIQR(),
+            gf.ACRange_per_axis(),
+            np.array([len(data)]),
+        ]
+    )
     return features
 
-def gyo(data):
-    return data[3]**2 + data[4]**2 + data[5]**2
 
-def run_micropython_script(port, filepath):
+def calculate_gyroscope_magnitude(data):
+    """
+    Calculates the sum of the squared magnitudes of the gyroscope data
+    """
+    return data[3] ** 2 + data[4] ** 2 + data[5] ** 2
+
+
+def main():
+    """
+    Runs the MicroPython script on the Arduino and process the output data.
+    """
     try:
-        print("run script...")
+        print("Running script...")
         # Run the MicroPython script using ampy
+        port = "/dev/cu.usbmodem0000000000001"  # USB Port of the Arduino
+        filepath = "src/record_data.py"  # Path to MicroPython script
         process = subprocess.Popen(
             ["ampy", "--port", port, "run", filepath],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            bufsize=1
+            bufsize=1,
         )
 
-        guesture_cut = []
-        flag = False
-        # Capture and process the output in real-time
+        gesture_data = []
+        gesture_in_progress = False
+
         with process.stdout as pipe:
             for line in pipe:
-                # print(line.strip())
-                data = line.strip().split(',')
-                data = [float(x) for x in data]
-                # print(data)
-                if guesture_cut == []:
-                    guesture_cut.append(data)
-                else:
-                    current_gyo = gyo(data)
-                    
-                    if current_gyo < thredhold and np.abs(gyo(guesture_cut[-1]) - current_gyo) < mang_change:
-                        if flag == True and min_gus_time <len(guesture_cut) < max_gus_time :
-                            guesture_cut = np.array(guesture_cut)
-                            features = getFeatures(guesture_cut)
-                            
-                            y_pred = clf.predict([features])
-                            print('guesture number is:',y_pred)
-                        flag = False
-                        guesture_cut = []
-                    else:
-                        flag = True
-                        guesture_cut.append(data)
+                try:
+                    # Parse the incoming data
+                    data = list(map(float, line.strip().split(",")))
 
+                    if not gesture_data:
+                        gesture_data.append(data)
+                    else:
+                        current_gyro_magnitude = calculate_gyroscope_magnitude(data)
+                        last_gyro_magnitude = calculate_gyroscope_magnitude(
+                            gesture_data[-1]
+                        )
+
+                        if (
+                            current_gyro_magnitude < GYRO_THRESHOLD
+                            and abs(last_gyro_magnitude - current_gyro_magnitude)
+                            < GYRO_CHANGE_THRESHOLD
+                        ):
+
+                            if (
+                                gesture_in_progress
+                                and MIN_GESTURE_TIME
+                                < len(gesture_data)
+                                < MAX_GESTURE_TIME
+                            ):
+                                features = extract_features(np.array(gesture_data))
+                                predicted_gesture_id = int(clf.predict([features])[0])
+                                predicted_gesture = GESTURES[predicted_gesture_id]
+                                print(
+                                    f"Detected {predicted_gesture} ({predicted_gesture_id})"
+                                )
+
+                            gesture_in_progress = False
+                            gesture_data = []
+                        else:
+                            gesture_in_progress = True
+                            gesture_data.append(data)
+                except ValueError:
+                    print("Warning: Received malformed data:", line.strip())
 
         process.wait()
         if process.returncode != 0:
-            raise subprocess.CalledProcessError(process.returncode, process.args, output=process.stdout, stderr=process.stderr)
+            raise subprocess.CalledProcessError(
+                process.returncode,
+                process.args,
+                output=process.stdout,
+                stderr=process.stderr,
+            )
 
     except Exception as e:
         print(f"Error: {e}")
 
 
-
 if __name__ == "__main__":
-    port = '/dev/cu.usbmodem0000000000001'  # Replace with the correct port for your system
-    filepath = 'src/record_data.py'  # Path to your Python script
-
-    run_micropython_script(port, filepath)
+    main()
